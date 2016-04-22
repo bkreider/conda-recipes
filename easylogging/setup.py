c62747a54d3aaeff865a2f0793eb2371d7e437e1
Bradley Kreider <bkreider@continuum.io>
2016-04-22 21:12:59 +0200

    added easylogging

---
+++ easylogging/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='easylogging',
+    version="1.0",
+    author='bkreider',
+    author_email='brad.kreider@gmail.com',
+    packages=["easylogging"],
+)
+
