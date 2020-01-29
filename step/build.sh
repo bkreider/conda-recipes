7e3650d004eadc37728b4d1c3b303b3f65e43255
Bradley Kreider <user.email=1699478+bkreider@users.noreply.github.com>
2020-01-29 18:20:49 -0500

    Added step* tools

---
+++ step/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Setup GOPATH crap
+export GOPATH="${SRC_DIR}"
+GO_BINDATA_SRC="$GOPATH/src/github.com/smallstep/cli"
+mkdir -p $GO_BINDATA_SRC
+cp -r "${SRC_DIR}"/* "${GO_BINDATA_SRC}" || true
+cp -r "${SRC_DIR}"/.* "${GO_BINDATA_SRC}" || true
+
+# go compiles its system libraries and fails on netdb.h otherwise 
+# don't know why
+export CGO_ENABLED=0
+
+pushd "${GO_BINDATA_SRC}"
+# Need this for deps
+export PATH=./bin:$PATH
+
+export GOBIN=$PREFIX/bin
+mkdir $PREFIX/bin
+
+make bootstrap
+make binary-darwin
+
+mv output/binary/darwin/bin/step $PREFIX/bin/step
+
+# GO installs modules without write preventing conda-build from cleaning up
+chmod u+w $GOPATH
+
